feb5a4951c662b586529183d6e5ff0a96504ad2d
Godrich-13 <139610724+Godrich-13@users.noreply.github.com>
2024-09-24 21:39:18 +0530

    ok

--- BrandrdXMusic/core/userbot.py
+++ BrandrdXMusic/core/userbot.py
@@ -71,7 +71,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BRANDED_WORLD")
+                await self.one.join_chat("vczone")
                 await self.one.join_chat("BRANDED_PAID_CC")
                 await self.one.join_chat("vczone")
                 await self.one.join_chat("ABOUT_BRANDEDKING")
@@ -101,7 +101,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
+                await self.two.join_chat("vczone")
                 await self.two.join_chat("BRANDED_PAID_CC")
                 await self.two.join_chat("vczone")
                 await self.two.join_chat("ABOUT_BRANDEDKING")
@@ -125,7 +125,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BRANDED_WORLD")
+                await self.three.join_chat("vczone")
                 await self.three.join_chat("BRANDED_PAID_CC")
                 await self.three.join_chat("vczone")
                 await self.three.join_chat("ABOUT_BRANDEDKING")
@@ -148,7 +148,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BRANDED_WORLD")
+                await self.four.join_chat("vczone")
                 await self.four.join_chat("BRANDED_PAID_CC")
                 await self.four.join_chat("vczone")
                 await self.four.join_chat("ABOUT_BRANDEDKING")
@@ -171,7 +171,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BRANDED_WORLD")
+                await self.five.join_chat("vczone")
                 await self.five.join_chat("BRANDED_PAID_CC")
                 await self.five.join_chat("vczone")
                 await self.five.join_chat("ABOUT_BRANDEDKING")
